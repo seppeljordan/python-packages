5d5397cb1453d20dcd0a2b275ee437bb27761ff8
Sebastian Jordan <sebastian.jordan.mail@googlemail.com>
2021-02-13 16:37:05 +0100

    Add parsemon2 to python packages

--- flake.nix
+++ flake.nix
@@ -4,11 +4,17 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    parsemon2.url = "github:seppeljordan/parsemon2";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, flake-utils, parsemon2 }:
     let
-      packageOverrides = import ./package-overrides.nix;
+      packageOverrides = let
+        parsemonOverrides = self: super: {
+          parsemon2 = self.callPackage parsemon2.lib.package { };
+        };
+        selfOverrides = import ./package-overrides.nix;
+      in nixpkgs.lib.composeExtensions parsemonOverrides selfOverrides;
       overlay = final: prev: {
         python3 = prev.python3.override { inherit packageOverrides; };
       };
@@ -22,7 +28,7 @@
           packages = { inherit (pkgs) python3; };
           checks = {
             inherit (pkgs.python3.pkgs)
-              injector pytest-profiling bumpv;
+              injector pytest-profiling bumpv parsemon2;
           };
         });
     in { inherit overlay packageOverrides; } // systemDependent;
